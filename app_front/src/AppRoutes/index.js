978d413716c6a2113a07fca5720063fc1d0841e3
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-04-09 23:01:11 +0000

    setup add modify monument routes

--- app_front/src/AppRoutes/index.js
+++ app_front/src/AppRoutes/index.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes ,useLocation} from "react-router-dom";
+import { BrowserRouter, Route, Routes ,useLocation,useParams} from "react-router-dom";
 import DashBoard from "../Pages/DashBoard";
 import { AnimatePresence } from 'framer-motion';
-import { useParams } from 'react-router-dom';
 import Monument from "../Pages/Monument";
 import Utilisateur from "../Pages/Utilisateur";
+import AjouterM from "../Pages/Monument/ajouterMonument";
+import ModifierM from "../Pages/Monument/modifierMonument";
 
 function AppRoutes(){
 
@@ -15,7 +16,13 @@ function AppRoutes(){
         <Routes >
           {/* Dashbaord*/}
           <Route path="/" element={<DashBoard />}></Route>
+
+          {/* Monument*/}
           <Route path="/monument" element={<Monument />}></Route>
+          <Route path="/monument/ajouterM" element={<AjouterM/>}></Route>
+          <Route path="/monument/modifierM" element={<ModifierM />}></Route>
+
+          {/* Utilisateur*/}
           <Route path="/utilisateur" element={<Utilisateur />}></Route>
           </Routes>
         </AnimatePresence>
