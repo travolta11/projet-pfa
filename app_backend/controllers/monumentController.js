0b176c2ee276e1cd12dda43e3abd525524adc236
issam liqali <63265720+travolta11@users.noreply.github.com>
2024-04-18 20:46:42 +0000

    ajouter monument done

--- app_backend/controllers/monumentController.js
+++ app_backend/controllers/monumentController.js
@@ -20,24 +20,46 @@ class MonumentController {
 
 
 //add controller................................................................
-    static async addMonument(req,res){
-            var titre=  req.body.titre;
-            var description = req.body.description;
-            var localisation =  req.body.localisation;
-            var ville =  req.body.ville;
-            var id_admin =  req.body.id_admin;
-            var createur =  req.body.createur;
-            var horaire =  req.body.horaire;
-            var frais = req.body.frais;
-            var avis = req.body.avis;
-            var images = req.body.images;
-        var addedM = await Monument.addMonuments(titre,description,localisation,ville,id_admin,createur,horaire,frais,avis,images)
-            if(addedM==true)
-            res.send('add succesfully')
-                else
-                
-                res.send('add failed')
+static async addMonument(req, res) {
+    try {
+      const {
+        titre,
+        description,
+        localisation,
+        ville,
+        id_admin,
+        createur,
+        horaire,
+        frais,
+        avis
+      } = req.body;
+  
+      const images = req.files.map(file => file.path);
+  
+      const addedM = await Monument.addMonuments(
+        titre,
+        description,
+        localisation,
+        ville,
+        id_admin,
+        createur,
+        horaire,
+        frais,
+        avis,
+        images
+      );
+  
+      if (addedM) {
+        res.send('Monument added successfully');
+      } else {
+        res.status(500).send('Failed to add monument');
+      }
+    } catch (error) {
+      console.error('Error adding monument:', error);
+      res.status(500).send('Internal Server Error');
     }
+  }
+  
 //add controller................................................................
 
 
