c354313f45ba1437774a633665a561f450459410
charafeddinejaafari <charafjaafari49@gmail.com>
2024-05-02 21:26:47 +0100

    Login-SignUp frontend

--- app_mobile/App.js
+++ app_mobile/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-export {default} from './Pages/Login/index';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import Login from './Pages/Login';
-
+import LoginForm from './Pages/Login';
+import SignUp from './Pages/Inscription/SignUp';
 
 const Stack = createStackNavigator();
 
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-      
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginForm} />
+        <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-
+export default App;
