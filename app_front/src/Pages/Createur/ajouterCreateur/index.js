fc90e6c779237dffb9db3edb6894b8b5308e348e
charafeddinejaafari <charafjaafari49@gmail.com>
2024-05-15 09:04:11 +0100

    Interface createur

--- app_front/src/Pages/Createur/ajouterCreateur/index.js
+++ app_front/src/Pages/Createur/ajouterCreateur/index.js
@@ -1,13 +1,67 @@
-import React from 'react';
-import { Typography } from 'antd';
+import React from "react";
+import { Typography, Form, Input, DatePicker, Button } from "antd";
+
 const { Title } = Typography;
-const AjouterCreateur = () => (
-  <>
-    <Title>h1. Ant Design</Title>
-    <Title level={2}>h2. Ant Design</Title>
-    <Title level={3}>h3. Ant Design</Title>
-    <Title level={4}>h4. Ant Design</Title>
-    <Title level={5}>h5. Ant Design</Title>
-  </>
-);
+
+const AjouterCreateur = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Received values of form:", values);
+  };
+
+  return (
+    <>
+      <Title>Ajouter un créateur</Title>
+      <Form
+        form={form}
+        name="createur_form"
+        onFinish={onFinish}
+        autoComplete="off"
+        layout="vertical"
+      >
+        <Form.Item
+          name="name"
+          label="Nom"
+          rules={[
+            { required: true, message: "Veuillez saisir le nom du créateur" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="biography"
+          label="Biographie"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez saisir la biographie du créateur",
+            },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item
+          name="date_of_birth"
+          label="Date de naissance"
+          rules={[
+            {
+              required: true,
+              message: "Veuillez saisir la date de naissance du créateur",
+            },
+          ]}
+        >
+          <DatePicker />
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit" className="submit-button">
+          Ajouter le créateur
+        </Button>
+      </Form>
+    </>
+  );
+};
+
 export default AjouterCreateur;
